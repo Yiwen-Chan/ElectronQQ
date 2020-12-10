14fbecfc5c036d09252fce113e62da99a9bb320c
Hanbings <3219065882@qq.com>
2020-12-10 21:29:04 +0800

    :sparkles: Feat: Mirai QQBot

--- src/main/java/com/nanokylin/ElectronQQ.java
+++ src/main/java/com/nanokylin/ElectronQQ.java
@@ -1,13 +1,20 @@
 package com.nanokylin;
 
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.BotFactoryJvm;
+import net.mamoe.mirai.utils.BotConfiguration;
+
 /**
  * Hello world!
- *
  */
-public class ElectronQQ
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class ElectronQQ {
+    public static void main(String[] args) {
+        final Bot bot = BotFactoryJvm.newBot(Long.parseLong("3284933889"), "", new BotConfiguration() {
+            {
+                fileBasedDeviceInfo("deviceInfo.json");
+            }
+        });
+        bot.login();
+        System.out.println("Hello World!");
     }
 }
